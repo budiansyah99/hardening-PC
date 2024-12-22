d84455661eeb2b3f05da14ff77425104a0414f48
budiansyah99 <61037741+budiansyah99@users.noreply.github.com>
2024-12-23 06:39:49 +0700

    Update setup_harden_centos.sh

--- setup_harden_centos.sh
+++ setup_harden_centos.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-# 1. Disable root login SSH
-echo "Disabling root login for SSH..."
-sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+# Disable root login via SSH
+
+# Memeriksa apakah pengguna memiliki hak akses root (sudo)
+if [ "$(id -u)" -ne "0" ]; then
+    echo "Anda harus menjalankan script ini sebagai root atau dengan sudo."
+    exit 1
+fi
+
+# Menonaktifkan root login di SSH
+echo "Menonaktifkan root login melalui SSH..."
+
+# Mengedit konfigurasi SSH untuk menonaktifkan login root
+sed -i 's/^#PermitRootLogin .*/PermitRootLogin no/' /etc/ssh/sshd_config
+echo "Root login via SSH telah dinonaktifkan."
+
+# Restart layanan SSH agar perubahan diterapkan
 systemctl restart sshd
+
+# Verifikasi apakah perubahan sudah diterapkan
+if grep -q "PermitRootLogin no" /etc/ssh/sshd_config; then
+    echo "Root login via SSH telah berhasil dinonaktifkan."
+else
+    echo "Gagal menonaktifkan root login via SSH."
+fi
 
 # 2. Add allow firewall Linux CentOS port 22, 443, 80
 echo "Configuring firewall rules..."
